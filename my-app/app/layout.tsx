077d0199b45c50d812acb9e8d56b5f4324da5b29
unknown <cbuzdugan19@gmail.com>
2023-07-28 22:09:41 +0300

    first commit

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from "@/components/navbar";
+import "./global.css";
+import Footer from "@/components/footer";
+
+export const metadata = {
+  title: 'Flexibble',
+  description: 'Showcase and discover remarkable developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
